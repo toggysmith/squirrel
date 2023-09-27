f64a2709cf7776d2304b9b3e97aaf042a4c1c7be
Toggy Smith <toggysmith@gmail.com>
2023-09-28 00:04:11 +0100

    Add fmt library and use it to format and colour headings
    This adds the fmt library as a dependency. It then uses fmt to make
    the code for printing headings nicer by using `fmt::print()` and it
    also makes use of fmt colouring to print the headings in a nice
    green colour so that they can be distinguished from other text.

--- src/io.cpp
+++ src/io.cpp
@@ -1,5 +1,8 @@
 #include "io.hpp"
 
+#include <fmt/color.h>
+#include <fmt/format.h>
+
 #include <iostream>
 #include <string>
 
@@ -9,8 +12,12 @@ void IO::print_heading(const std::string_view heading_message) {
   const std::string heading_border{
       std::string(heading_message_length + 4, heading_border_symbol)};
 
-  std::cout << heading_border << '\n';
-  std::cout << heading_border_symbol << ' ' << heading_message << ' '
-            << heading_border_symbol << '\n';
-  std::cout << heading_border << '\n' << '\n';
+  fmt::print(
+      fg(fmt::color::green_yellow),
+      "{heading_border}\n"
+      "{heading_border_symbol} {heading_message} {heading_border_symbol}\n"
+      "{heading_border}\n",
+      fmt::arg("heading_border", heading_border),
+      fmt::arg("heading_border_symbol", heading_border_symbol),
+      fmt::arg("heading_message", heading_message));
 }
