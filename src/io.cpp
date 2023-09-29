3acd8a377abdfd5580c58419953a7254d688fce8
Toggy Smith <toggysmith@gmail.com>
2023-09-29 01:16:36 +0100

    Add more spacing around H1 headings
    This adds an additional layer of spacing around the inner text of
    all H1 headings in order to further distinguish them from H2 headings.

--- src/io.cpp
+++ src/io.cpp
@@ -11,22 +11,49 @@ void IO::print_heading(const std::string_view heading_message,
                        const HeadingType heading_type) {
   const std::size_t heading_message_length{heading_message.size()};
   const char heading_border_symbol{'#'};
-  const std::string heading_border{
-      std::string(heading_message_length + 4, heading_border_symbol)};
   const static std::unordered_map<HeadingType, fmt::color> heading_colors{
       {HeadingType::H1, fmt::color::green_yellow},
       {HeadingType::H2, fmt::color::gold},
   };
   const fmt::color heading_color{heading_colors.at(heading_type)};
 
-  fmt::print(
-      fg(heading_color),
-      "{heading_border}\n"
-      "{heading_border_symbol} {heading_message} {heading_border_symbol}\n"
-      "{heading_border}\n\n",
-      fmt::arg("heading_border", heading_border),
-      fmt::arg("heading_border_symbol", heading_border_symbol),
-      fmt::arg("heading_message", heading_message));
+  switch (heading_type) {
+  case HeadingType::H1: {
+    const std::string heading_border{
+        std::string(heading_message_length + 6, heading_border_symbol)};
+    const std::string heading_spacing{
+        std::string(heading_message_length + 4, ' ')};
+
+    fmt::print(
+        fg(heading_color),
+        "{heading_border}\n"
+        "{heading_border_symbol}{heading_spacing}{heading_border_symbol}\n"
+        "{heading_border_symbol}  {heading_message}  {heading_border_symbol}\n"
+        "{heading_border_symbol}{heading_spacing}{heading_border_symbol}\n"
+        "{heading_border}\n\n",
+        fmt::arg("heading_border", heading_border),
+        fmt::arg("heading_spacing", heading_spacing),
+        fmt::arg("heading_border_symbol", heading_border_symbol),
+        fmt::arg("heading_message", heading_message));
+    break;
+  }
+  default: {
+    const std::string heading_border{
+        std::string(heading_message_length + 4, heading_border_symbol)};
+    const std::string heading_spacing{
+        std::string(heading_message_length + 2, ' ')};
+
+    fmt::print(
+        fg(heading_color),
+        "{heading_border}\n"
+        "{heading_border_symbol} {heading_message} {heading_border_symbol}\n"
+        "{heading_border}\n\n",
+        fmt::arg("heading_border", heading_border),
+        fmt::arg("heading_border_symbol", heading_border_symbol),
+        fmt::arg("heading_message", heading_message));
+    break;
+  }
+  }
 }
 
 const std::string IO::get_text_input(const std::string_view prompt) {
